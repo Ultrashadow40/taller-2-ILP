76f2f05fd25aa4fda4cadee383426e9ee4c96a5b
Ultrashadow40 <102318197+Ultrashadow40@users.noreply.github.com>
2025-03-05 18:56:05 -0500

    Add files via upload

---
+++ punto 1.py
@@ -0,0 +1,5 @@
+celsius = float(input("Ingresa la temperatura en grados Celsius: ")) #Se ingresa la temperatura en grados Celsius
+fahrenheit = (celsius * 9/5) + 32 #Se convierte la temperatura a grados Fahrenheit
+kelvin = celsius + 273.15 #Se convierte la temperatura a grados Kelvin
+print(f"Temperatura en Fahrenheit: {fahrenheit:.2f} °F") #Se imprime la temperatura en grados Fahrenheit
+print(f"Temperatura en Kelvin: {kelvin:.2f} K") #Se imprime la temperatura en grados Kelvin
